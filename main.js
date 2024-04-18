7d348c4a042f80d627245c3f454bb84656f9e0d5
Vlaad1955 <wylf1312@gmail.com>
2024-04-18 15:23:35 +0300

    add HW1

--- main.js
+++ main.js
@@ -1,1 +1,36 @@
-console.log("hello")
+const fs = require('fs').promises;
+const path = require("path");
+
+async function check(y){
+    try {
+        const stats = await fs.stat(y);
+        if(stats.isFile()){
+            return "Файл"
+        }else {
+            return "Папка"
+        }
+    } catch (err) {
+        console.error(`Помилка: ${err}`);
+    }
+}
+async function builder(){
+    try{
+        for (let i=0; i<5; i++) {
+            const name = `folder${i+1}`;
+            const dirFolder = path.join(__dirname, "baseFolder", name);
+            await fs.mkdir(dirFolder, {recursive: true});
+            console.log(`${await check(dirFolder)} ${name}: ${dirFolder}`)
+            for (let x=0; x<5; x++){
+                const nameFile = `File${x+1}.txt`
+                const dirFile = path.join(__dirname, "baseFolder", name, nameFile);
+                await fs.writeFile(dirFile,"");
+                console.log(`${await check(dirFile)} ${nameFile}: ${dirFile}`)
+            }
+        }
+
+    } catch (e){
+        console.log(e)
+    }
+}
+
+void builder();
